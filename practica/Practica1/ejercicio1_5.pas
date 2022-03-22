fd69cb578c1a27f55c704becff86075d3051059c
MarianoGonzalez98 <68166494+MarianoGonzalez98@users.noreply.github.com>
2022-03-22 17:36:44 -0300

    done in home

--- practica/Practica1/ejercicio1_5.pas
+++ practica/Practica1/ejercicio1_5.pas
@@ -20,6 +20,14 @@ tres líneas consecutivas: en la primera se especifica: código de celular, el p
 marca, en la segunda el stock disponible, stock mínimo y la descripción y en la tercera
 nombre en ese orden. Cada celular se carga leyendo tres líneas del archivo
 “celulares.txt”
+* 
+* 
+6. Agregar al menú del programa del ejercicio 5, opciones para:
+* 
+a. Añadir uno o más celulares al final del archivo con sus datos ingresados por teclado.
+b. Modificar el stock de un celular dado.
+c. Exportar el contenido del archivo binario a un archivo de texto denominado: ”SinStock.txt”, con aquellos celulares que tengan stock 0.
+NOTA: Las búsquedas deben realizarse por nombre de celular.
 }
 program ejercicio1_5;
 type
@@ -84,7 +92,6 @@ procedure listarPorDescripcion(var arc_file:fCelulares);
 		readln(cad);
 		while not eof(arc_file) do begin
 			read(arc_file,r);
-//Comparar para ver si CONTIENE
 			if cad=r.desc then 
 				with r do
 					writeln(cod,' ',precio,' ',marca,' ',stockDisp,' ',stockMin,' ',desc,' ',nombre);
@@ -94,10 +101,98 @@ procedure listarPorDescripcion(var arc_file:fCelulares);
 	end;
 
 procedure exportarTxt(var arc_file:fCelulares; var arc_text:text);
+	var
+		r:rCelular;
 	begin
+		reset (arc_file);
+		rewrite(arc_text);
+		while not eof(arc_file) do begin
+			read(arc_file,r);
+			writeln(arc_text,r.cod,' ',r.precio:2:2,' ',r.marca);
+			writeln(arc_text,r.stockDisp,' ',r.stockMin,' ',r.desc);
+			writeln(arc_text,r.nombre);
+		end;
+		close (arc_file);
+		close(arc_text);
+	end;
+{a. Añadir uno o más celulares al final del archivo con sus datos ingresados por teclado}
+
+procedure leer(var r:rCelular);
+	begin
+		writeln('Ingrese cod de celular. Finalice con -1');	readln(r.cod);
+		if r.cod<>-1 then begin
+			writeln('precio: '); readln(r.precio);
+			writeln('marca: '); readln(r.marca);
+			writeln('stock Disponible: '); readln(r.stockDisp);
+			writeln('stock minimo: '); readln(r.stockMin);
+			writeln('descripcion: '); readln(r.desc);
+			writeln('nombre: '); readln(r.nombre);
+		end;
+	end;
+procedure anadirCelulares(var arc_file:fCelulares);
+	var
+		r:rCelular;
+	begin
+		reset (arc_file);
+		seek(arc_file,filesize(arc_file)); //va al final del archivo
+		leer(r);
+		while r.cod<>-1 do begin
+			write(arc_file,r);
+			leer(r);
+		end;
+		close (arc_file);
+	end;
+	
+	{b. Modificar el stock de un celular dado.}
+procedure modificarStockCelu(var arc_file:fCelulares);
+	var
+		cod,stock:integer;
+		encontro: boolean;
+		r:rCelular;
+	begin
+		reset(arc_file);
+		writeln('Ingrese el cod del celular a modificar');
+		readln(cod);
+		
+		encontro:=false;
+		while (not eof(arc_file)) and not encontro do begin
+			read(arc_file,r);
+			if r.cod = cod then encontro:=true;
+		end;
+		if encontro then begin
+			writeln('Ingrese nuevo stock');
+			readln(stock);
+			r.stockDisp:= stock;
+			seek(arc_file,filepos(arc_file)-1);
+			write(arc_file,r);
+		end
+		else writeln('Codigo incorrecto');
+		close(arc_file);
+	end;
+//c. Exportar el contenido del archivo binario a un archivo de texto denominado: ”SinStock.txt”, con aquellos celulares que tengan stock 0.
+procedure exportarSinStock(var arc_file:fCelulares);
+	var
+		r:rCelular;
+		arc_textNoStock: text;
+	begin
+		assign(arc_textNoStock, 'SinStock.txt');
+		rewrite(arc_textNoStock);
+		reset(arc_file);
+		
+		while not eof(arc_file) do begin
+			read(arc_file,r);
+			if r.stockDisp=0 then begin
+				writeln(arc_textNoStock,r.cod,' ',r.precio:2:2,' ',r.marca);
+				writeln(arc_textNoStock,r.stockDisp,' ',r.stockMin,' ',r.desc);
+				writeln(arc_textNoStock,r.nombre);
+			end;
+		end;
+		
+		
+		close(arc_text);
+		close(arc_file);
 	
 	end;
-
 var
 	arc_file:fCelulares;
 	arc_text:text;
@@ -112,6 +207,9 @@ begin
 	writeln('2 para listar en pantalla los datos de aquellos celulares que tengan un stock menor al stock mínimo.');
 	writeln('3 para listar en pantalla los celulares del archivo cuya descripción contenga una cadena de caracteres proporcionada por el usuario.' );
 	writeln('4 para Exportar el archivo de registros a un archivo de texto “celulares.txt” con todos los celulares del mismo');
+	writeln('5 para Añadir uno o más celulares al final del archivo con sus datos ingresados por teclado.');
+	writeln('6 para Modificar el stock de un celular dado.');
+	writeln('7 para Exportar el contenido del archivo binario a un archivo de texto denominado: ”SinStock.txt”, con aquellos celulares que tengan stock 0.');
 	readln(opcion);
 	
 	case opcion of
@@ -119,6 +217,9 @@ begin
 		'2': listarStockMenor(arc_file);
 		'3': listarPorDescripcion(arc_file);
 		'4': exportarTxt(arc_file,arc_text);
+		'5': anadirCelulares(arc_file);
+		'6': modificarStockCelu(arc_file);
+		'7': exportarSinStock(arc_file,arc_text);
 		else writeln('Opcion incorrecta');
 	end;
 end.
